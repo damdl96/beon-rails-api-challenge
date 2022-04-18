baebacfb3e0c722f869be26916a6e2f6c12d98f5
Diego Moreno <diegodm96@hotmail.com>
2022-04-18 09:03:29 -0500

    Get all tweets by user and base tweets#index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,15 @@
 class TweetsController < ApplicationController
   def index
-    render json: Tweet.all
+    tweets = Tweet
+              .get_by_user(params[:user_username])
+              .get_cronologically
+
+    render json: tweets.all
+  end
+
+  private
+
+  def permitted_params
+    params.permit(:user_username)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  scope :get_cronologically, -> { order(created_at: :DESC) }
+  scope :get_by_user,        -> (username) { joins(:user).where(users: { username: username }) }
 end
