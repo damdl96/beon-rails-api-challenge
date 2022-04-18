70477a1e365985cba22e7bc900a6da06471d9402
Diego Moreno <diegodm96@hotmail.com>
2022-04-18 08:51:36 -0500

    enhance seach by username using any pattern

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'scopes' do
+    context 'find by_username' do
+      it 'returns all users that match' do
+        create(:user)
+        expect(User.by_username('us')).to_not be_nil
+      end
+    end
+  end
 end
